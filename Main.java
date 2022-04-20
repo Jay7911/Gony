cfb1820a1cfbae131013d9d8d2159009321eb09b
Jay7911 <jaehyun482@gmail.com>
2022-04-21 02:09:40 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+//import Test
+
+
+public class Main {
+  public static void main(String[] args) {
+    float x=12e3f;
+    double y=45E3d;
+    String s="Hello World!";
+    System.out.println(x);
+    System.out.println(y);
+    //System.out.println(Test.method1);
+    System.out.println("s="+s);
+  }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    int field1;
+    String field2;
+
+    public void method1(){
+        System.out.println("Java Programming!");
+    }
+    
+}
